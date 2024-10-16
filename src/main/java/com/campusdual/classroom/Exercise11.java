638086c7b6b30e8c1da200bddcc20c83397fc2b3
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-16 16:57:33 +0200

    Terminado Exercise11

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -2,10 +2,50 @@ package com.campusdual.classroom;
 
 public class Exercise11 {
     public static void main(String[] args) {
+
+
         TVRemote redRemote = new TVRemote("Rojo");
         TVRemote blackRemote = new TVRemote("Negro");
-        System.out.println(redRemote.getColor());
-        System.out.println(blackRemote.getColor());
+
+        //Encender la TV
+        redRemote.turnOn();
+        blackRemote.turnOn();
+
+        //Subir un canal
+        redRemote.channelUp();
+        blackRemote.channelUp();
+
+        //Bajar un canal
+        redRemote.channelDown();
+        blackRemote.channelDown();
+
+        //Establecer el canal en 0, intentar bajar un canal y comprobar que no se puede
+        redRemote.channel = 0; //establecer el canal a 0
+        redRemote.channelDown(); //intentar bajar un canal en 0
+        System.out.println("Canal después de intentar bajar: " + redRemote.channel); //deberia seguir en 0
+
+        //Subir el volumen
+        redRemote.volumeUp();
+        blackRemote.volumeUp();
+
+        //Bajar el volume
+        redRemote.volumeDown();
+        blackRemote.volumeDown();
+
+        //Establecer el volumen en 0, intentar bajar el volumen y comprobar que no se puede
+        redRemote.volume = 0;
+        redRemote.volumeDown();
+        System.out.println("Volumen después de intentar bajar: " + redRemote.volume);
+
+        //Obtener el color del mando a distancia y mostrarlo por pantalla
+        System.out.println("Color del control remoto rojo: " + redRemote.getColor());
+        System.out.println("Color del control remoto negro: " + blackRemote.getColor());
+
+        //Apagar la TV
+        redRemote.turnOff();
+        blackRemote.turnOff();
+
     }
+
 
 }
